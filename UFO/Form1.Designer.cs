ce525f3a094ff8ab6517889ea6f244f988db3720
ryans610 <ryans610@gmail.com>
2015-09-08 15:40:02 +0800

    UFO

---
+++ UFO/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace UFO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.UFO = new System.Windows.Forms.PictureBox();
+            this.bar = new System.Windows.Forms.PictureBox();
+            this.Reset = new System.Windows.Forms.Button();
+            this.ScorePanel = new System.Windows.Forms.Label();
+            this.Pause = new System.Windows.Forms.Button();
+            this.SpeedUp = new System.Windows.Forms.Button();
+            this.SpeedDown = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.UFO)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // UFO
+            // 
+            this.UFO.Image = ((System.Drawing.Image)(resources.GetObject("UFO.Image")));
+            this.UFO.Location = new System.Drawing.Point(63, 53);
+            this.UFO.Name = "UFO";
+            this.UFO.Size = new System.Drawing.Size(50, 50);
+            this.UFO.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.UFO.TabIndex = 0;
+            this.UFO.TabStop = false;
+            // 
+            // bar
+            // 
+            this.bar.Image = ((System.Drawing.Image)(resources.GetObject("bar.Image")));
+            this.bar.Location = new System.Drawing.Point(160, 500);
+            this.bar.Name = "bar";
+            this.bar.Size = new System.Drawing.Size(400, 5);
+            this.bar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bar.TabIndex = 2;
+            this.bar.TabStop = false;
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(68, 29);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(75, 23);
+            this.Reset.TabIndex = 5;
+            this.Reset.TabStop = false;
+            this.Reset.Text = "重新開始";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // ScorePanel
+            // 
+            this.ScorePanel.AutoSize = true;
+            this.ScorePanel.Font = new System.Drawing.Font("新細明體", 10F);
+            this.ScorePanel.Location = new System.Drawing.Point(12, 12);
+            this.ScorePanel.Name = "ScorePanel";
+            this.ScorePanel.Size = new System.Drawing.Size(41, 14);
+            this.ScorePanel.TabIndex = 4;
+            this.ScorePanel.Text = "label1";
+            // 
+            // Pause
+            // 
+            this.Pause.Location = new System.Drawing.Point(12, 29);
+            this.Pause.Name = "Pause";
+            this.Pause.Size = new System.Drawing.Size(50, 23);
+            this.Pause.TabIndex = 3;
+            this.Pause.TabStop = false;
+            this.Pause.Text = "暫停";
+            this.Pause.UseVisualStyleBackColor = true;
+            this.Pause.Click += new System.EventHandler(this.Pause_Click);
+            // 
+            // SpeedUp
+            // 
+            this.SpeedUp.Location = new System.Drawing.Point(149, 29);
+            this.SpeedUp.Name = "SpeedUp";
+            this.SpeedUp.Size = new System.Drawing.Size(23, 23);
+            this.SpeedUp.TabIndex = 6;
+            this.SpeedUp.TabStop = false;
+            this.SpeedUp.Text = "+";
+            this.SpeedUp.UseVisualStyleBackColor = true;
+            this.SpeedUp.Click += new System.EventHandler(this.SpeedUp_Click);
+            // 
+            // SpeedDown
+            // 
+            this.SpeedDown.Location = new System.Drawing.Point(178, 29);
+            this.SpeedDown.Name = "SpeedDown";
+            this.SpeedDown.Size = new System.Drawing.Size(23, 23);
+            this.SpeedDown.TabIndex = 7;
+            this.SpeedDown.TabStop = false;
+            this.SpeedDown.Text = "-";
+            this.SpeedDown.UseVisualStyleBackColor = true;
+            this.SpeedDown.Click += new System.EventHandler(this.SpeedDown_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.SpeedDown);
+            this.Controls.Add(this.SpeedUp);
+            this.Controls.Add(this.Pause);
+            this.Controls.Add(this.ScorePanel);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.bar);
+            this.Controls.Add(this.UFO);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "UFO";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.UFO)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox UFO;
+        private System.Windows.Forms.PictureBox bar;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Label ScorePanel;
+        private System.Windows.Forms.Button Pause;
+        private System.Windows.Forms.Button SpeedUp;
+        private System.Windows.Forms.Button SpeedDown;
+    }
+}
+
---
+++ UFO/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace UFO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Initialize();
+            timer1.Enabled = true;
+        }
+
+        //初始化
+        private void Initialize()
+        {
+            surviveTime = 0;
+            stepX = 1;
+            stepY = 1;
+            speed = 1;
+            UFO.Location = new Point((int)(this.Width * 0.2), (int)(this.Height * 0.2));
+            PanelUpdate();
+        }
+
+        //參數設定
+        private int speed = 1;
+        private int stepX = 1;
+        private int stepY = 1;
+        private double surviveTime = 0;
+
+        //文字設定
+        private void PanelUpdate()
+        {
+            ScorePanel.Text = "存活時間：" + surviveTime.ToString("0.00") + "秒 速度：" + speed.ToString();
+        }
+
+        //timer
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //移動
+            UFO.Location = new Point(UFO.Location.X + stepX * speed, UFO.Location.Y + stepY * speed);
+            //反彈
+            if (UFO.Location.Y + UFO.Height > bar.Location.Y &&
+                UFO.Location.Y < bar.Location.Y &&
+                UFO.Location.X > bar.Location.X - UFO.Width &&
+                UFO.Location.X < bar.Location.X + bar.Width)
+            {
+                stepY = -stepY;
+            }
+            if (UFO.Location.X < 0 || UFO.Location.X + UFO.Width > this.Width)
+            {
+                stepX = -stepX;
+            }
+            if (UFO.Location.Y < 0)
+            {
+                stepY = -stepY;
+            }
+            //分數計算
+            surviveTime += (double)timer1.Interval / 1000;
+            PanelUpdate();
+            //死亡判斷
+            if (UFO.Location.Y + UFO.Height > this.Height)
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("你死了!\n存活時間：" + surviveTime.ToString("0.00") + "秒");
+                Initialize();
+            }
+        }
+
+        //bar move
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            RePositionBar();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            RePositionBar();
+        }
+
+        private void RePositionBar()
+        {
+            bar.Location = new Point(MousePosition.X - bar.Width / 2, (int)(this.Height * 0.8));
+        }
+
+        //pause
+        private void Pause_Click(object sender, EventArgs e)
+        {
+            DoPause();
+            ScorePanel.Focus();
+        }
+
+        private void DoPause()
+        {
+            if (timer1.Enabled)
+            {
+                Pause.Text = "繼續";
+                timer1.Enabled = false;
+            }
+            else
+            {
+                Pause.Text = "暫停";
+                timer1.Enabled = true;
+            }
+        }
+
+        //speed control
+        private void SpeedUp_Click(object sender, EventArgs e)
+        {
+            SpeedChange(1);
+            ScorePanel.Focus();
+        }
+
+        private void SpeedDown_Click(object sender, EventArgs e)
+        {
+            SpeedChange(-1);
+            ScorePanel.Focus();
+        }
+
+        private void SpeedChange(int value)
+        {
+            speed += value;
+            if (speed <= 0)
+            {
+                speed = 1;
+            }
+        }
+
+        //reset
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            Initialize();
+            ScorePanel.Focus();
+        }
+
+        //key control
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Space:
+                    DoPause();
+                    break;
+                case Keys.Escape:
+                    timer1.Enabled = false;
+                    MessageBox.Show("存活時間：" + surviveTime.ToString("0.00") + "秒");
+                    this.Close();
+                    break;
+                case Keys.Add:
+                    SpeedChange(1);
+                    break;
+                case Keys.Subtract:
+                    SpeedChange(-1);
+                    break;
+            }
+        }
+    }
+}
